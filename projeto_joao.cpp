2f058b24617917cc92f416a3429b19fe92eb380e
jppsilv <151471092+jppsilv@users.noreply.github.com>
2023-12-19 23:47:41 -0300

    upload do projeto
    Projeto completo de acordo com as especificações do cliente

---
+++ projeto_joao.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h> // bliblioteca de comunicação com o usuario
+#include <stdlib.h> // biblioteca de alocação de espaço de memoria 
+#include <locale.h> // bliblioteca de alocação de texto por região
+
+int main()
+{
+	int opcao=0; // definindo as variaveis
+	int laco=1;
+	
+	for (laco=1;laco=1;)
+{
+
+	   setlocale(LC_ALL, "Portuguese"); //defiindo a linguagem
+	    
+	   printf("### Cartório da EBAC ### \n\n");  //inicio do menu
+	   printf("escolha a opção desejada do menu:\n\n");
+	   printf("\t1 - Registra nomes\n");
+	   printf("\t2 - Consultar nomes\n");
+	   printf("\t3 - Deletar nomes\n\n");  
+	   printf("opcao");//final do menu
+	   
+	  scanf("%d", &opcao); //armazenando a escolha do usúsario
+	 
+	  system("cls");
+	  
+	  switch(opcao)
+	  {
+	  	case 1:
+	  	printf("voce escolheu o registro de nomes!\n");
+	  	system("pause");
+	    break;
+	  	  
+	  	case 2:
+	  	printf("voce escolheu consultar nomes!\n");
+        system("pause");
+        break;
+        	
+        case 3:
+        printf("voce escolheu deletar  nomes!\n");
+	  	system("pause");
+	  	break;
+	  	
+	  	default:
+	    printf("Essa opcao nao esta disponivel!\n");
+	    system("pause");
+	    break;
+	  
+        		
+	  }
+	  
+	  
+}
+}
